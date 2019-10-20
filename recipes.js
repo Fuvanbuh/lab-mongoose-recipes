198e55ca4cbee191da44dee08cb7bd4b4f0e28cf
Fuvanbuh <gerardfunes@telefonica.net>
2019-10-20 20:24:06 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
+const Recipies = require('./models/Recipies');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +9,33 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+/*Recipies.create({ title: 'Tartar', level: 'UltraPro Chef', cuisine: 'Mediterranean' })
+  .then(recipe => { console.log(`The ${recipe.title} recipe was created`) })
+  .catch(err => {
+    console.log(`An Error ocurred ${err}. `)
+  });*/
+
+/*Recipies.insertMany(data)
+  .then(recipes => { console.log(`this is ${recipes}`) })
+  .catch(err => {
+    console.log(`An Error ocurred ${err}.`)
+  });*/
+
+/*Recipies.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }, (err, doc) => {
+  if (err) {
+    console.log('Something went wrong.')
+  } else {
+    console.log(doc)
+  }
+})*/
+
+Recipies.remove({ title: "tartar" }, (err, doc) => {
+  if (err) {
+    console.log('Something went wrong.')
+  } else {
+    console.log(`${doc} was removed.`)
+  }
+})
+
+mongoose.connection.close()
